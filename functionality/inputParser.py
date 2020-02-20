9fcfac2183087ac035f7bc933574e70384a7298f
Nemanja Jevtic <njevtic998@gmail.com>
2020-02-20 23:40:29 +0100

    #parsiranje_skupa_HTML_dokumenata Prebacivanje implementacije u zaseban fajl

---
+++ functionality/inputParser.py
@@ -0,0 +1,22 @@
+
+def parseInput(string):
+    array = string.split(' ')
+    validInput = False
+    logicalInput = False
+    if len(array) == 3:
+        if array[1].upper() == 'AND' or array[1].upper() == 'OR' or array[1].upper() == 'NOT':
+           validInput = True
+           logicalInput = True
+        else:
+           validInput = True
+    if len(array) == 3 and ((array[0].upper() == 'AND' or array[0].upper() == 'OR' or array[0].upper() == 'NOT') or (array[2].upper() == 'AND' or array[2].upper() == 'OR' or array[2].upper() == 'NOT')):
+        validInput = False
+    if len(array) !=3:
+        validInput = True
+        for i in range(len(array)):
+            if array[i].upper() == 'AND' or array[i].upper() == 'OR' or array[i].upper() == 'NOT':
+                validInput = False
+                break
+
+    return array,validInput,logicalInput
+
